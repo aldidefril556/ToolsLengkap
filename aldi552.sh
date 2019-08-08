db6837928771456095f70ce9c995700399a0b1dc
aldidefril556 <53865241+aldidefril556@users.noreply.github.com>
2019-08-08 19:32:36 +0700

    aldi552.sh

--- aldi552.sh
+++ aldi552.sh
@@ -1,75 +1,76 @@
-n#!/bin/bash
-clear
-bi='\033[34;1m' #biru
-i='\033[32;1m' #ijo
-pur='\033[35;1m' #purple
-cy='\033[36;1m' #cyan
-me='\033[31;1m' #merah
-pu='\033[37;1m' #purple
-ku='\033[33;1m' #kuning
-echo
-echo
+  
+n # ! / bin / bash
+bersih
+bi = ' \ 033 [34; 1m '  # biru
+i = ' \ 033 [32; 1m '  # ijo
+pur = ' \ 033 [35; 1m '  # ungu
+cy = ' \ 033 [36; 1m '  # cyan
+saya = ' \ 033 [31; 1m '  # merah
+pu = ' \ 033 [37; 1m '  # ungu
+ku = ' \ 033 [33; 1m '  # kuning
+gema
+gema
 python2 meizu.py
-echo
-clear
-echo
-echo $red"          Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa ("
-sleep 1
-echo '''\a
-\033[34;1m                              _
-\033[34;1m                             / |
-\033[34;1m                             | |
-\033[34;1m                             | |
-\033[34;1m                            _|_|_ '''
-sleep 1
-apt update && apt upgrade
-apt install nano
-apt install git
-pkg install python python2 vim figlet curl
-clear
-echo
-echo
-echo
-echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa "
-sleep 1
-echo '''\a
-\033[32;1m                             ____
-\033[32;1m                            |___ \
-\033[32;1m                             __) |
-\033[32;1m                            / __/
-\033[32;1m                           |_____| '''
-sleep 1
-apt install php
-pip2 install mechanize
-pip2 install lolcat
-pip2 install requests
-clear
-echo
-echo
-echo
-echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa "
-sleep 1
-echo '''\a
-\033[35;1m                            _____
-\033[35;1m                           |___ /
-\033[35;1m                             |_ \
-\033[35;1m                            ___) |
-\033[35;1m                           |____/ '''
-sleep 1
-clear
-echo
-echo
-echo
-echo
-echo $red"         Tool akan di install dalam waktu 5 detik"
-echo $red"            harap sabar menunggu yaa "
-sleep 1
-echo '''\a
+gema
+bersih
+gema
+echo  $ red "           Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa ( "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [34; 1m _
+\ 033 [34; 1m / |
+\ 033 [34; 1m | |
+\ 033 [34; 1m | |
+\ 033 [34; 1m _ | _ | _ ' ' '
+tidur 1
+pembaruan apt && peningkatan apt
+untuk menginstal nano
+untuk menginstal git
+pkg instal python python2 vim figlet curl
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [32; 1m ____
+\ 033 [32; 1m | ___ \
+\ 033 [32; 1m __) |
+\ 033 [32; 1m / __ /
+\ 033 [32; 1m | _____ | ' ' '
+tidur 1
+untuk memasang php
+Instal pip2 mekanis
+pip2 instal lolcat
+permintaan instalasi pip2
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa "
+tidur 1
+gema  ' ' ' \ a
+\ 033 [35; 1m _____
+\ 033 [35; 1m | ___ /
+\ 033 [35; 1m | _ \
+\ 033 [35; 1m ___) |
+\ 033 [35; 1m | ____ / ' ' '
+tidur 1
+bersih
+gema
+gema
+gema
+gema
+echo  $ red "          Alat akan di instal dalam waktu 5 detik "
+echo  $ red "             harap tunggu menunggu yaa "
+tidur 1
+gema  ' ' ' \ a
 \033[33;1m                            _  _
 \033[33;1m                           | || |
 \033[33;1m                           | || |_
@@ -120,192 +121,193 @@ echo $ku"["$me"16"$ku"]"$i" LITEDDOS"
 echo $i"["$me"17"$i"]"$ku" EXIT"
 echo
 echo $pur"Ôò¡ÔöÇ["$pur"Masukkan pilihan anda"$pur"]"
-read -p"Ôò¦ÔöÇ>> " pil
+baca -p " Ôò¦ÔöÇ >> " pil
 
-if [ $pil = 1 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 1]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/IlayTamvan/Report.git
-cd Report
+laporan cd
 unzip Report.zip
 python2 Report.py
 fi
 
-if [ $pil = 2 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 2]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/Senitopeng/fbbrute.git
 cd fbbrute
 python2 jomblo.py
 fi
 
-if [ $pil = 3 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 3]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/FR13ND8/Fb-Cracker-v.3.git
 cd Fb-Cracker-v.3
 python2 crack.py
 fi
 
-if [ $pil = 4 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 4]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
 cd multi-bruteforce-facebook
 python2 MBF.py
 fi
 
-if [ $pil = 5 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 5]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/wahyuandhika/YahooCloning.git
 cd YahooCloning
-pip2 install requests mechanize
-pip2 install requirements
+permintaan instalasi pip2 memekanisasi
+persyaratan pemasangan pip2
 python2 cloning.py
 fi
 
-if [ $pil = 6 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 6]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/CiKu370/OSIF.git
 cd OSIF
-pip2 install -r requirements.txt
+pip2 install -r requirement.txt
 python2 osif.py
 fi
 
-if [ $pil = 7 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 7]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/jaxBCD/FBshield.git
 cd FBshield
 python2 guard.py
 fi
 
-if [ $pil = 8 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 8]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/edi-nasa12/ROBOT.git
 cd ROBOT
 python2 blackbot.py
 fi
 
-if [ $pil = 9 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 9]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/Aditya021/SpamCall
 cd SpamCall
 php SpamCall.php
 fi
 
-if [ $pil = 10 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 10]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/4L13199/LITESPAM.git
 cd LITESPAM
 sh LITESPAM.sh
 fi
 
-if [ $pil = 11 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 11]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/rahmadxyz/sub-bot.git
-cd sub-bot
+sub-bot cd
 php sub-bot.php
 fi
 
-if [ $pil = 12 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 12]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/thelinuxchoice/locator.git
 cd locator
 bash locator.sh
 fi
 
-if [ $pil = 13 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 13]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/TERMUXID3/brutal-sms
 cd brutal-sms
 python2 run.py
 fi
 
-if [ $pil = 14 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 14]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/Mr-XsZ/Dark-Fb
 cd Dark-Fb
 python2 dark.py
 fi
 
-if [ $pil = 15 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 15]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/maldevel/IPGeoLocation
 cd IPGeoLocation
-chmod +x ipgeolocation.py
-pip install -r requirements.txt
+chmod + x ipgeolocation.py
+pip instal -r requirement.txt
 python ipgeolocation.py -t 10.99.110.220
 fi
 
-if [ $pil = 16 ]
-then
-clear
-figlet -f slant "W A I T"|lolcat
-sleep 1
+if [ $ pil  = 16]
+kemudian
+bersih
+figlet -f miring " WAIT " | lolcat
+tidur 1
 git clone https://github.com/4L13199/LITEDDOS
 cd LITEDDOS
 python2 LITEDDOS.py
 fi
 
-if [ $pil = 17 ]
-then
-clear
-figlet -f slant "W A I T"|Ulolcat
-sleep 2
-echo $cy"Terima Kasih sudah mengunakan Tools ini"
-sleep 2
-echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
-sleep 2
-echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
-sleep 2
-echo $ku"Facebook :"$i" Aldi Defril"
-echo $ku"YouTube  :"$i" YouTube RMD"
-echo $ku"WhatsApp :"$i" 081268707053"
-sleep 2
-echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
-sleep 2
-echo $pur"SUBSCRIBE MY CHANNEL { YOUTUBE RMD }"
-exit
+if [ $ pil  = 17]
+kemudian
+bersih
+figlet -f miring " WAIT " | Ulolcat
+tidur 2
+echo  $ cy " Terima Kasih sudah mengunakan Alat ini "
+tidur 2
+echo  $ i " Silakan pakai semoga bermanfaat Buat kamu "
+tidur 2
+echo  $ pur "Jika Ada Kesalahan Anda Bisa Melalui Melalui Nanya "
+tidur 2
+echo  $ ku " Facebook: " $ i " Aldi Defril "
+echo  $ ku " YouTube: " $ i " YouTube RMD "
+echo  $ ku " WhatsApp: " $ i " 081268707053 "
+tidur 2
+echo  $ pur " Terima kasih Yang Sudah Mendukung dengan Tools ini "
+tidur 2
+echo  $ pur " BERLANGGANAN MY CHANNEL {YOUTUBE RMD} "
+keluar
 fi
+
 
